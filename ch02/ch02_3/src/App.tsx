a0e6cbe67ff1fe14ed47df4893fa1b08b8e50cb9
3ae3ae <ogyshjsm@naver.com>
2023-06-01 09:18:51 +0900

    화살표 함수 컴포넌트 구현

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import ClassComponent from './ClassComponent'
+import ArrowComponent from './ArrowComponent'
 
 function App() {
   return (
     <ul>
       <ClassComponent href="https://www.google.com" text="go to google"></ClassComponent>
-      <ClassComponent href="https://www.naver.com" text="go to naver"></ClassComponent>
+      <ArrowComponent href="https://www.naver.com" text="go to naver"></ArrowComponent>
     </ul>
   )
 }
